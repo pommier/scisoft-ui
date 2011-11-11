e8a45080090979eedd562d2ad2736512d14d97df
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2011-11-11 12:36:57 +0000

    SDA - plotting controls: new classes and .properties file that were missing from the previous commit 516e16ba51af6704c7cb563bed6b9bc04f41e359

---
+++ uk.ac.diamond.scisoft.analysis.rcp/src/uk/ac/diamond/scisoft/analysis/rcp/plotting/actions/PlotPrintGraphWithScale66Action.java
@@ -0,0 +1,49 @@
+/*-
+ * Copyright © 2011 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.diamond.scisoft.analysis.rcp.plotting.actions;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.printing.PrintDialog;
+import org.eclipse.swt.printing.PrinterData;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import uk.ac.diamond.scisoft.analysis.rcp.plotting.DataSetPlotter;
+import uk.ac.diamond.scisoft.analysis.rcp.views.PlotView;
+
+public class PlotPrintGraphWithScale66Action extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		final PlotView pv = (PlotView) HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().getActivePart();
+		DataSetPlotter plotter = pv.getMainPlotter();
+
+		if (plotter != null) {
+			
+			PrintDialog dialog = new PrintDialog(pv.getSite().getShell(), SWT.NULL);
+			PrinterData printerData = dialog.open();
+			plotter.printGraph(printerData, 0.66f);
+			//Printer pr =new Printer(data)
+		}
+		return Boolean.TRUE;
+	}
+
+}
---
+++ uk.ac.diamond.scisoft.analysis.rcp/src/uk/ac/diamond/scisoft/analysis/rcp/util/ResourceProperties.java
@@ -0,0 +1,77 @@
+/*-
+ * Copyright © 2011 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.diamond.scisoft.analysis.rcp.util;
+
+import java.text.MessageFormat;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import org.eclipse.swt.widgets.Composite;
+
+public class ResourceProperties {
+	private static ResourceBundle resourceBundle = ResourceBundle.getBundle("sda_resource"); //$NON-NLS-1$
+
+	/**
+	 * Creates an instance of a ControlExample embedded inside the supplied parent Composite.
+	 * 
+	 * @param parent
+	 *            the container of the example
+	 */
+	@SuppressWarnings("unused")
+	public ResourceProperties(Composite parent) {
+		initResources();
+	}
+
+	/**
+	 * Gets a string from the resource bundle. We don't want to crash because of a missing String. Returns the key if
+	 * not found.
+	 */
+	public static String getResourceString(String key) {
+		try {
+			return resourceBundle.getString(key);
+		} catch (MissingResourceException e) {
+			return key;
+		} catch (NullPointerException e) {
+			return "!" + key + "!"; //$NON-NLS-1$ //$NON-NLS-2$
+		}
+	}
+
+	/**
+	 * Gets a string from the resource bundle and binds it with the given arguments. If the key is not found, return the
+	 * key.
+	 */
+	static String getResourceString(String key, Object[] args) {
+		try {
+			return MessageFormat.format(getResourceString(key), args);
+		} catch (MissingResourceException e) {
+			return key;
+		} catch (NullPointerException e) {
+			return "!" + key + "!"; //$NON-NLS-1$ //$NON-NLS-2$
+		}
+	}
+
+	/**
+	 * Loads the resources
+	 */
+	void initResources() {
+		String error = (resourceBundle != null) ? getResourceString("error.CouldNotLoadResources")
+				: "Unable to load resources"; //$NON-NLS-1$
+		throw new RuntimeException(error);
+	}
+}
