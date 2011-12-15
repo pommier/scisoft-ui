e9223b5ba44a75e6c282471565bc5c98df0f8d9a
Matthew Gerring <fcp94556@DIAMRL5294.dc.diamond.ac.uk>
2011-12-15 11:55:34 +0000

    Made default dir user.home for FileView.
    Change-Id: Id758210893bd002caa7cb4330ca019f4e62e0e25

--- uk.ac.diamond.sda.navigator/src/uk/ac/diamond/sda/navigator/views/FileView.java
+++ uk.ac.diamond.sda.navigator/src/uk/ac/diamond/sda/navigator/views/FileView.java
@@ -76,9 +76,11 @@ public class FileView extends ViewPart {
 		
 		super.init(site, memento);
 		
-		if (memento==null) return;
-		final String path = memento.getString("DIR");
-		if (path!=null) {
+		String path = null;
+		if (memento!=null) path = memento.getString("DIR");
+		if (path==null) path = System.getProperty("user.home");
+		
+		if (path!=null){
 			savedSelection = new File(path);
 		}
 	}
