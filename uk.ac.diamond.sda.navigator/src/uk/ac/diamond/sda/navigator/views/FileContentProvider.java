a9702c32969c1aefcdd3b396e32bff3f11c79bc2
Matthew Gerring <fcp94556@DIAMRL5294.dc.diamond.ac.uk>
2011-12-16 16:39:37 +0000

    Added jobs to FileView which uses a lazy tree for viewing large file systems on lustre.
    Change-Id: Ie6cec0c48ed2feb1d203df9cb9ee8fdf8805c51e

--- uk.ac.diamond.sda.navigator/src/uk/ac/diamond/sda/navigator/views/FileContentProvider.java
+++ uk.ac.diamond.sda.navigator/src/uk/ac/diamond/sda/navigator/views/FileContentProvider.java
@@ -5,11 +5,20 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.WeakHashMap;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.viewers.ILazyTreeContentProvider;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Cursor;
+import org.eclipse.ui.PlatformUI;
 
+import uk.ac.gda.common.rcp.util.EclipseUtils;
 import uk.ac.gda.util.io.SortingUtils;
 
 public class FileContentProvider implements ILazyTreeContentProvider {
@@ -20,7 +29,7 @@ public class FileContentProvider implements ILazyTreeContentProvider {
 	
 	private TreeViewer treeViewer;
 	private FileSortType sort = FileSortType.ALPHA_NUMERIC_DIRS_FIRST;
-
+    private BlockingQueue<UpdateRequest> queue;
 
 	/**
 	 * Caching seems to be needed to keep the path sorting
@@ -30,7 +39,8 @@ public class FileContentProvider implements ILazyTreeContentProvider {
 	private Map<File, List<File>> cachedSorting;
 
 	public FileContentProvider() {
-		cachedSorting=new WeakHashMap<File, List<File>>(89);
+		this.cachedSorting = new WeakHashMap<File, List<File>>(89);
+		this.queue         = new LinkedBlockingDeque<UpdateRequest>(89);
 	}
 
 	
@@ -47,9 +57,76 @@ public class FileContentProvider implements ILazyTreeContentProvider {
 
 	@Override
 	public void updateElement(Object parent, int index) {
-		File node = (File) parent;
+
+		if (PlatformUI.isWorkbenchRunning()) {
+			createUpdateElementJobIfNotAlreadyGoing();
+			queue.add(new UpdateRequest(parent, index));
+		} else {
+			final File node = (File) parent;
+			final List<File> fa = getFileList(node);
+			updateElementInternal(node, index, fa);
+		}
+	}
+	
+	private Job updateElementJob;
+	
+	/**
+	 * Somewhat long method name, but you get the idea.
+	 */
+	private void createUpdateElementJobIfNotAlreadyGoing() {
 		
-		final List<File> fa = getFileList(node);
+		if (updateElementJob==null) {
+			updateElementJob = new Job("Update directory contents") {
+				
+				private Cursor busy = treeViewer.getControl().getDisplay().getSystemCursor(SWT.CURSOR_WAIT);
+				@Override
+				protected IStatus run(IProgressMonitor monitor) {
+					while(true) {
+                        try {
+
+                    		final UpdateRequest req = queue.take();
+                    		treeViewer.getControl().getDisplay().syncExec(new Runnable() {
+								@Override
+								public void run() {
+									treeViewer.getControl().setCursor(busy);
+								}
+                			});
+
+                			final File node = (File) req.getElement();
+                			final List<File> fa = getFileList(node);
+                			
+                			treeViewer.getControl().getDisplay().asyncExec(new Runnable() {
+								@Override
+								public void run() {
+									updateElementInternal(node, req.getIndex(), fa);
+								}
+                			});
+
+                        } catch (Exception ne) {
+                        	queue.clear();
+                        	continue;
+                        } finally {
+                        	treeViewer.getControl().getDisplay().syncExec(new Runnable() {
+								@Override
+								public void run() {
+									treeViewer.getControl().setCursor(null);
+								}
+                			});
+                        }
+					}
+				}	
+			};
+			updateElementJob.setSystem(true);
+			updateElementJob.setUser(false);
+			updateElementJob.schedule(100);
+
+		}
+	}
+
+
+	public void updateElementInternal(Object parent, int index, List<File> fa) {
+		
+		
 		if (fa!=null && index < fa.size()) {
 			File element = fa.get(index);
 			treeViewer.replace(parent, index, element);
@@ -88,6 +165,14 @@ public class FileContentProvider implements ILazyTreeContentProvider {
 			return cachedSorting.get(node);
 		}
 		
+		// Simulate Lustreless
+//		try {
+//			Thread.sleep(10000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
 		final List<File> sorted;
 		if (sort==FileSortType.ALPHA_NUMERIC) {
 			sorted = SortingUtils.getSortedFileList(node, false);
@@ -116,6 +201,32 @@ public class FileContentProvider implements ILazyTreeContentProvider {
 
 	public void setSort(FileSortType sort) {
 		this.sort = sort;
+	}
+	
+	private class UpdateRequest {
+		private Object element;
+		private int index;
+
+		public Object getElement() {
+			return element;
+		}
+
+		public void setElement(Object element) {
+			this.element = element;
+		}
+
+		public int getIndex() {
+			return index;
+		}
+
+		public void setIndex(int index) {
+			this.index = index;
+		}
+
+		UpdateRequest(final Object element, final int index) {
+			this.element = element;
+			this.index   = index;
+		}
 	}
 
 }
