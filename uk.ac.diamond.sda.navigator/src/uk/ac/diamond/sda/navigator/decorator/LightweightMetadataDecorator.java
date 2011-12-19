989da46e569ba4f93ab4d7efe9eaa56a8690930b
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2011-12-19 14:50:23 +0000

    SCISOFT-UI - SDA navigator: ILabelDecorator changed to ILightweightLabelDecorator to enable faster display of files in the project explorer

---
+++ uk.ac.diamond.sda.navigator/src/uk/ac/diamond/sda/navigator/decorator/LightweightMetadataDecorator.java
@@ -0,0 +1,90 @@
+/*-
+ * Copyright © 2011 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.diamond.sda.navigator.decorator;
+
+import java.io.File;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.viewers.LabelProvider;
+
+import org.eclipse.jface.viewers.IDecoration;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.jface.viewers.ILightweightLabelDecorator;
+
+
+public class LightweightMetadataDecorator extends LabelProvider implements ILightweightLabelDecorator {
+
+	public LightweightMetadataDecorator() {
+		super();
+	}
+
+	@Override
+	public void addListener(ILabelProviderListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isLabelProperty(Object element, String property) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void removeListener(ILabelProviderListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void decorate(Object element, IDecoration decoration) {
+		IResource objectResource = (IResource) element;
+		if (element instanceof IFile) {
+			// Decorate the label of the resource with the admin name
+			IFile ifile = (IFile) element;
+			IPath path = ifile.getLocation();
+			File file = path.toFile();
+			objectResource.getResourceAttributes().toString();
+
+			String lastModified = new SimpleDateFormat("dd/MM/yy hh:mm aaa").format(new Date(file.lastModified()));
+			
+			decoration.addSuffix("  " + readableFileSize(file.length()) + "  " + lastModified);
+			//decoration.setForegroundColor(Display.getCurrent().getSystemColor(SWT.COLOR_CYAN));
+		}
+	}
+	
+	public static String readableFileSize(long size) {
+		if (size <= 0)
+			return "0";
+		final String[] units = new String[] { "B", "KB", "MB", "GB", "TB" };
+		int digitGroups = (int) (Math.log10(size) / Math.log10(1024));
+		return new DecimalFormat("#,##0.#").format(size / Math.pow(1024, digitGroups)) + " " + units[digitGroups];
+	}
+}
---
+++ uk.ac.diamond.sda.navigator/src/uk/ac/diamond/sda/navigator/decorator/LightweightScanCommandDecorator.java
@@ -0,0 +1,201 @@
+/*-
+ * Copyright © 2011 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.diamond.sda.navigator.decorator;
+
+import gda.analysis.io.ScanFileHolderException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.viewers.LabelProvider;
+
+import org.eclipse.jface.viewers.IDecoration;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.jface.viewers.ILightweightLabelDecorator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.ac.diamond.scisoft.analysis.io.DataHolder;
+import uk.ac.diamond.scisoft.analysis.io.HDF5Loader;
+import uk.ac.diamond.scisoft.analysis.io.IExtendedMetadata;
+import uk.ac.diamond.scisoft.analysis.io.IMetaData;
+import uk.ac.diamond.scisoft.analysis.io.LoaderFactory;
+
+public class LightweightScanCommandDecorator extends LabelProvider implements ILightweightLabelDecorator {
+
+	private static final Object SRS_EXT = "dat"; //$NON-NLS-1$
+	private static final Object H5_EXT = "h5"; //$NON-NLS-1$
+	private static final Object HDF5_EXT = "hdf5"; //$NON-NLS-1$
+	private static final Object NXS_EXT = "nxs"; //$NON-NLS-1$
+	private IExtendedMetadata metaData;
+	private String decorator = "";
+	private String fileName;
+	private static final Logger logger = LoggerFactory.getLogger(LightweightScanCommandDecorator.class);
+
+	public LightweightScanCommandDecorator() {
+		super();
+	}
+
+	@Override
+	public void addListener(ILabelProviderListener listener) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public boolean isLabelProperty(Object element, String property) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public void removeListener(ILabelProviderListener listener) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void decorate(Object element, IDecoration decoration) {
+		decorator = "";
+		if (element instanceof IFile) {
+			IFile modelFile = (IFile) element;
+			if (SRS_EXT.equals(modelFile.getFileExtension())) {
+				IFile ifile = (IFile) element;
+				// IPath path = ifile.getLocation();
+				// File file = path.toFile();
+				srsMetaDataLoader(ifile);
+
+				try {
+					//Collection<String> list = metaData.getMetaNames();
+					decorator = metaData.getScanCommand();
+					if(decorator==null){
+						decorator="Scan Command: N/A";
+						decoration.addSuffix(decorator);
+						//decoration.setForegroundColor(Display.getCurrent().getSystemColor(SWT.COLOR_BLUE));
+					}else{
+						if (decorator.length() > 100) // restrict to 100 characters
+							decorator = decorator.substring(0, 100) + "...";
+						decorator = "* " + decorator;
+						decoration.addSuffix(decorator);
+						//decoration.setForegroundColor(Display.getCurrent().getSystemColor(SWT.COLOR_BLUE));
+					}
+				}catch (Exception e) {
+					logger.error("Could not read metadata: ", e);
+				}
+
+			}
+			if (NXS_EXT.equals(modelFile.getFileExtension())) {
+				IFile ifile = (IFile) element;
+
+				try {
+					System.out.println("");
+					String[][] listTitlesAndScanCmd = getHDF5TitleAndScanCmd(ifile);
+					for (int i = 0; i < listTitlesAndScanCmd[0].length; i++) {
+						decorator = listTitlesAndScanCmd[0][i] + listTitlesAndScanCmd[1][i];
+						decoration.addSuffix(decorator);
+						//decoration.setForegroundColor(Display.getCurrent().getSystemColor(SWT.COLOR_BLUE));
+					}
+				} catch (ScanFileHolderException e) {
+					logger.error("Could not read hdf5 file: ", e);
+				}catch (Exception e){
+					logger.error("Could not read hdf5metadata: ", e);
+				}
+			}
+		}		
+	}
+	
+	private void srsMetaDataLoader(IFile file) {
+		fileName = file.getLocation().toString();
+		
+		try {
+			IMetaData metaDataTest=LoaderFactory.getMetaData(fileName, null);
+			if(metaDataTest instanceof IExtendedMetadata)
+				metaData = (IExtendedMetadata)LoaderFactory.getMetaData(fileName, null);
+			else{
+				decorator=" Scan Command: N/A";
+				logger.warn("Cannot decorate SRS decorator");
+			}
+		} catch (Exception ne) {
+			logger.error("Cannot open dat file", ne);
+		}
+	}
+
+	private String[][] getHDF5TitleAndScanCmd(IFile file) throws Exception {
+		fileName = file.getLocation().toString();
+		String hdf5scanCommand = "";
+		String hdf5Title = "";
+
+		//HDF5File data = new HDF5Loader(fileName).loadTree();
+		DataHolder dataHolder= new HDF5Loader(fileName).loadFile();
+
+		List<String> list = getAllRootEntries(dataHolder.getNames());
+		Object[] entries = list.toArray();
+		String[] scanCmd = new String[entries.length];
+		scanCmd=initStringArray(scanCmd);
+		String[] titles = new String[entries.length];
+		titles=initStringArray(titles);
+		
+		String[][] listScanCmdAndTitles = new String[2][entries.length];
+		for (int i = 0; i < entries.length; i++) {
+			// scan command
+			if (dataHolder.contains("/" + entries[i].toString() + "/scan_command")) {
+				//hdf5scanCommand = data.findNodeLink("/" + entries[i].toString() + "/scan_command").toString();
+				hdf5scanCommand = dataHolder.getDataset("/" + entries[i].toString() + "/scan_command").toString();
+				scanCmd[i] = "\nScanCmd" + (i+1) + ": " + hdf5scanCommand;// display of the string on a new line
+			}
+			// title
+			if (dataHolder.contains("/" + entries[i].toString() + "/title")) {
+				System.out.println(dataHolder.getDataset("/" + entries[i].toString() + "/title").toString());
+				//hdf5Title = data.findNodeLink("/" + entries[i].toString() + "/title").toString();
+				hdf5Title = dataHolder.getDataset("/" + entries[i].toString() + "/title").toString();
+				titles[i] = "\nTitle" + (i+1) + ": " + hdf5Title;// display of the string on a new line
+			}
+			if (titles[i].length() > 100) // restrict to 100 characters
+				titles[i] = titles[i].substring(0, 100) + "...";
+			if (scanCmd[i].length() > 100) // restrict to 100 characters
+				scanCmd[i] = scanCmd[i].substring(0, 100) + "...";
+			
+			listScanCmdAndTitles[0][i] = titles[i];
+			listScanCmdAndTitles[1][i] = scanCmd[i];
+		}
+		return listScanCmdAndTitles;
+
+	}
+	
+	public String[] initStringArray(String[] array){
+		for (int i = 0; i < array.length; i++) {
+			array[i]="";
+		}
+		return array;
+	}
+
+	public List<String> getAllRootEntries(String[] oldFullPaths) {
+		List<String> list = new ArrayList<String>();
+		for (int i = 0; i < oldFullPaths.length; i++) {
+			String[] tmp = oldFullPaths[i].split("/");
+			if (!list.contains(tmp[1]))
+				list.add(tmp[1]);
+		}
+		return list;
+	}
+}
