f9763bbfab4fd187e348388675727357c9e3c724
Peter Chang <Peter.Chang@diamond.ac.uk>
2012-01-20 11:48:35 +0000

    SCISOFT-UI: fix to catch NPE when metadata is wrong in diffraction images

--- uk.ac.diamond.scisoft.analysis.rcp/src/uk/ac/diamond/scisoft/analysis/rcp/plotting/DataSetPlotter.java
+++ uk.ac.diamond.scisoft.analysis.rcp/src/uk/ac/diamond/scisoft/analysis/rcp/plotting/DataSetPlotter.java
@@ -1100,7 +1100,7 @@ public class DataSetPlotter extends JPanel implements ComponentListener, IObserv
 						qSpace = new QSpace(diffnMetadata.getDetector2DProperties(),
 								diffnMetadata.getDiffractionCrystalEnvironment());
 						isDiffImage = true;
-					} catch (IllegalArgumentException e) {
+					} catch (Exception e) {
 						logger.debug("Could not create a detector properties object from metadata");
 					}
 				}
