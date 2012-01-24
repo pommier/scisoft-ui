0ddcbba123d9f51ca3c2ce851fbfa6e7bf06dc46
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2012-01-24 17:12:37 +0000

    SCISOFT-UI - Plot Printing: * display of the file name above the plot * various cleaning up

--- uk.ac.diamond.scisoft.analysis.rcp/src/uk/ac/diamond/scisoft/analysis/rcp/plotting/Plot1DStackUI.java
+++ uk.ac.diamond.scisoft.analysis.rcp/src/uk/ac/diamond/scisoft/analysis/rcp/plotting/Plot1DStackUI.java
@@ -30,6 +30,7 @@ import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.IWorkbenchPage;
 
 import uk.ac.diamond.scisoft.analysis.dataset.AbstractDataset;
 import uk.ac.diamond.scisoft.analysis.plotserver.AxisMapBean;
@@ -46,6 +47,7 @@ import uk.ac.diamond.scisoft.analysis.rcp.preference.PreferenceConstants;
  */
 public class Plot1DStackUI extends AbstractPlotUI {
 
+	private IWorkbenchPage page;
 	private AxisValues xAxis = null;
 	private AxisValues zAxis = null;
 	private DataSetPlotter mainPlotter;
@@ -76,9 +78,10 @@ public class Plot1DStackUI extends AbstractPlotUI {
 	public Plot1DStackUI(final PlotWindow window,
 						 IActionBars bars, 
 						 final DataSetPlotter plotter,
-						 Composite parent) {
+						 Composite parent, IWorkbenchPage page) {
 
 		this.parent = parent;
+		this.page = page;
 		this.plotWindow = window;
 		xAxis = new AxisValues();
 		zAxis = new AxisValues();
@@ -185,6 +188,9 @@ public class Plot1DStackUI extends AbstractPlotUI {
 					mainPlotter.replaceAllPlots(datasets);
 				} catch (PlotException ex) {}
 			}
+			
+			//we set the plot/file name
+			mainPlotter.setTitle(page.getActivePart().getTitle());
 	
 			parent.getDisplay().asyncExec(new Runnable() {
 				@Override
