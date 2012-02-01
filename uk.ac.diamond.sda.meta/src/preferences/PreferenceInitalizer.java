7266da154344c88ef42daed52c560bea930bd32a
Duncan Sneddon <Duncan.Sneddon@diamond.ac.uk>
2012-02-01 14:12:21 +0000

    METADATA VIEW - adding code for preference page
    Change-Id: Iad1dbc53f2357032311c4ac90d0fbec2a1b62538

---
+++ uk.ac.diamond.sda.meta/src/preferences/PreferenceInitalizer.java
@@ -0,0 +1,13 @@
+package preferences;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+
+public class PreferenceInitalizer extends AbstractPreferenceInitializer {
+
+	@Override
+	public void initializeDefaultPreferences() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
