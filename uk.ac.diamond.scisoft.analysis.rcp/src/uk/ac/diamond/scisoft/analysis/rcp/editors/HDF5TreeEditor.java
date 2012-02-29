b929534363bfd0c9d05a904c67d2dbc8a7614622
Peter Chang <Peter.Chang@diamond.ac.uk>
2012-02-29 11:55:02 +0000

    SCISOFT-UI: fix issue to do with testing for old GDA written files

--- uk.ac.diamond.scisoft.analysis.rcp/src/uk/ac/diamond/scisoft/analysis/rcp/editors/HDF5TreeEditor.java
+++ uk.ac.diamond.scisoft.analysis.rcp/src/uk/ac/diamond/scisoft/analysis/rcp/editors/HDF5TreeEditor.java
@@ -122,7 +122,6 @@ public class HDF5TreeEditor extends EditorPart implements IPageChangedListener, 
 		IWorkbenchPartSite site = getSite();
 		hdfxp = new HDF5TreeExplorer(parent, site, null);
 		if (!loadHDF5Tree()) {
-			
 			return;
 		}
 		site.setSelectionProvider(hdfxp);
@@ -292,7 +291,7 @@ public class HDF5TreeEditor extends EditorPart implements IPageChangedListener, 
 	 * This is used on the workflow perspective to show selected value in the tree.
 	 */
 	@Override
-    public Object getAdapter(final Class clazz) {
+    public Object getAdapter(@SuppressWarnings("rawtypes") final Class clazz) {
 		
 		if (clazz == IContentProvider.class) {
 			return new HDF5ValuePage();
