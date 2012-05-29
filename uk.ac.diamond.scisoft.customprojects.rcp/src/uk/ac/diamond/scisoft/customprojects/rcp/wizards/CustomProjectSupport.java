f3f63121dcd1d7154d2bc6b9c5765c48b0c1fc85
Ghita Kouadri Mostefaoui <ghita.mostefaoui@diamond.ac.uk>
2012-05-29 17:17:16 +0100

    non-recursive project: first import

---
+++ uk.ac.diamond.scisoft.customprojects.rcp/src/uk/ac/diamond/scisoft/customprojects/rcp/wizards/CustomProjectSupport.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2012 Diamond Light Source Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.ac.diamond.scisoft.customprojects.rcp.wizards;
+
+import java.net.URI;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.ac.diamond.scisoft.customprojects.rcp.natures.SingleLevelProjectNature;
+
+
+public class CustomProjectSupport {
+	
+	private static final Logger logger = LoggerFactory.getLogger(CustomProjectSupport.class);
+
+	
+    public static IProject createProject(String projectName, URI location) {
+        Assert.isNotNull(projectName);
+        Assert.isTrue(projectName.trim().length() > 0 );
+        
+        IProject project = createBaseProject(projectName, location);
+        try {
+            logger.debug("adding nature");
+        	addNature(project);
+            
+            //String[] paths = { "parent/child1-1/child2", "parent/child1-2/child2/child3" }; //$NON-NLS-1$ //$NON-NLS-2$
+            //addToProjectStructure(project, paths);        	
+        	
+        } catch (CoreException e) {
+            e.printStackTrace();
+            project = null;
+        }
+
+        return project;
+    }
+
+    /**
+     * Just do the basics: create a basic project.
+     *
+     * @param location
+     * @param projectName
+     */
+    private static IProject createBaseProject(String projectName, URI location) {
+    	logger.debug("projectName= " + projectName + "  location= " + location);
+        
+   	
+    	// it is acceptable to use the ResourcesPlugin class
+        IProject newProject = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
+
+        if (!newProject.exists()) {
+            URI projectLocation = location;
+
+            IProjectDescription desc = newProject.getWorkspace().newProjectDescription(newProject.getName());
+            if (location != null && ResourcesPlugin.getWorkspace().getRoot().getLocationURI().equals(location)) {
+				projectLocation = null;
+			}
+                       
+            if(projectLocation != null){
+            desc.setLocationURI(projectLocation);
+            }
+            
+            logger.debug("projectLocation: " + projectLocation.getPath());
+            try {
+                newProject.create(desc, null);
+                
+                if (!newProject.isOpen()) {
+                    newProject.open(null);
+                }
+            } catch (CoreException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        return newProject;
+    }
+
+//    private static void createFolder(IFolder folder) throws CoreException {
+//        IContainer parent = folder.getParent();
+//        if (parent instanceof IFolder) {
+//            createFolder((IFolder) parent);
+//        }
+//        if (!folder.exists()) {
+//            folder.create(false, true, null);
+//        }
+//    }
+
+    /**
+     * Create a folder structure with a parent root, overlay, and a few child
+     * folders.
+     *
+     * @param newProject
+     * @param paths
+     * @throws CoreException
+     */
+//    private static void addToProjectStructure(IProject newProject, String[] paths) throws CoreException {
+//        for (String path : paths) {
+//            IFolder etcFolders = newProject.getFolder(path);
+//            createFolder(etcFolders);
+//        }
+//    }
+
+    private static void addNature(IProject project) throws CoreException {
+        if (!project.hasNature(SingleLevelProjectNature.NATURE_ID)) {
+            IProjectDescription description = project.getDescription();
+            String[] prevNatures = description.getNatureIds();
+            String[] newNatures = new String[prevNatures.length + 1];
+            System.arraycopy(prevNatures, 0, newNatures, 0, prevNatures.length);
+            newNatures[prevNatures.length] = SingleLevelProjectNature.NATURE_ID;
+            description.setNatureIds(newNatures);
+
+            IProgressMonitor monitor = null;
+            project.setDescription(description, monitor);
+        }
+    }
+
+}
---
+++ uk.ac.diamond.scisoft.customprojects.rcp/src/uk/ac/diamond/scisoft/customprojects/rcp/wizards/NonRecursiveWizard.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright 2012 Diamond Light Source Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.ac.diamond.scisoft.customprojects.rcp.wizards;
+
+import java.io.File;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.dialogs.IDialogSettings;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchWizard;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.ac.diamond.scisoft.customprojects.rcp.internal.CustomProjectActivator;
+import uk.ac.diamond.scisoft.customprojects.rcp.natures.SingleLevelProjectNature;
+
+
+public class NonRecursiveWizard extends Wizard implements INewWizard {
+	private static final String NON_RECURSIVE_WIZARD = "NonRecursiveWizard";
+	public static final String DIALOG_SETTING_KEY_DIRECTORY = "directory";
+	public static final String DIALOG_SETTING_KEY_FOLDER = "folder";
+	public static final String DIALOG_SETTING_KEY_PROJECT = "project";
+	private static final Logger logger = LoggerFactory.getLogger(NonRecursiveWizard.class);	
+	private NonRecursiveWizardPage page;
+	private ISelection selection;
+	private String defaultDataLocation, defaultFolderName;
+
+	/**
+	 * Constructor for TestWizard.
+	 */
+	public NonRecursiveWizard() {
+		super();
+		setNeedsProgressMonitor(true);
+		IDialogSettings dialogSettings = CustomProjectActivator.getDefault().getDialogSettings();
+		IDialogSettings section = dialogSettings.getSection(NON_RECURSIVE_WIZARD);
+		if(section == null){
+			section = dialogSettings.addNewSection(NON_RECURSIVE_WIZARD);
+		}
+		setDialogSettings(section);
+	}
+
+	/**
+	 * Adding the page to the wizard.
+	 */
+	@Override
+	public void addPages() {
+		String prevProject = null , prevFolder = null, prevDirectory = null;
+		IDialogSettings  settings = getDialogSettings();
+		if( settings != null){
+			prevProject = settings.get(DIALOG_SETTING_KEY_PROJECT);
+			prevFolder = settings.get(DIALOG_SETTING_KEY_FOLDER);
+			prevDirectory = settings.get(DIALOG_SETTING_KEY_DIRECTORY);
+		}
+		if (defaultDataLocation!=null) {
+			prevDirectory = defaultDataLocation;
+		}
+		if (defaultFolderName!=null) {
+			prevFolder = defaultFolderName;
+		}
+		
+		page = new NonRecursiveWizardPage(selection, prevProject, prevFolder, prevDirectory);
+		addPage(page);
+	}
+
+	/**
+	 * This method is called when 'Finish' button is pressed in
+	 * the wizard. We will create an operation and run it
+	 * using wizard as execution context.
+	 */
+	@Override
+	public boolean performFinish() {
+	
+		final String project = page.getProject();
+		final String directory = page.getDirectory();
+		final String folder = page.getFolder();
+
+		final Job loadDataProject = new Job("Load data project") {
+
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				monitor.beginTask("Importing data", 100);
+				try {
+					NonRecursiveProjectUtils.createImportProjectAndFolder(project, folder, directory, SingleLevelProjectNature.NATURE_ID, null, monitor);
+				} catch (CoreException e) {
+					logger.error("Error creating project " + project, e);
+					return new Status(IStatus.ERROR, CustomProjectActivator.PLUGIN_ID, "Error creating project " + project);
+				}
+				return new Status(IStatus.OK, CustomProjectActivator.PLUGIN_ID, "Project " + project + " created");
+			}
+		};
+
+		loadDataProject.setUser(true);
+		loadDataProject.setPriority(Job.DECORATE);
+		loadDataProject.schedule(100);
+		
+
+		IDialogSettings settings = getDialogSettings();
+		if( settings != null){
+			settings.put(DIALOG_SETTING_KEY_PROJECT, project);
+			settings.put(DIALOG_SETTING_KEY_FOLDER, folder);
+			settings.put(DIALOG_SETTING_KEY_DIRECTORY, directory);
+		}
+		return true;
+	}
+
+	/**
+	 * We will accept the selection in the workbench to see if
+	 * we can initialize from it.
+	 * @see IWorkbenchWizard#init(IWorkbench, IStructuredSelection)
+	 */
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		this.selection = selection;
+	}
+
+	public void setDataLocation(File selectedPath) {
+		this.defaultDataLocation = selectedPath.getAbsolutePath();
+		this.defaultFolderName   = selectedPath.getName();
+	}
+}
