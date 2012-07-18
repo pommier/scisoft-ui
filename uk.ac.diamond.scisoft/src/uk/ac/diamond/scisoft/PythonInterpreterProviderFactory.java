0d7fac497bcdfc7bf3a9e667f674587e65dd71fc
Peter Chang <Peter.Chang@diamond.ac.uk>
2012-07-18 10:15:23 +0100

    Add and correct copyright notices

--- uk.ac.diamond.scisoft/src/uk/ac/diamond/scisoft/PythonInterpreterProviderFactory.java
+++ uk.ac.diamond.scisoft/src/uk/ac/diamond/scisoft/PythonInterpreterProviderFactory.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012 Diamond Light Source Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.ac.diamond.scisoft;
 
 import org.python.pydev.core.REF;
@@ -21,8 +37,7 @@ public class PythonInterpreterProviderFactory extends AbstractInterpreterProvide
 			return null;
 		}
 
-		// This should be enough to find it from the PATH or any other way it's
-		// defined.
+		// this resolves eventually to python on PATH
 		return AlreadyInstalledInterpreterProvider.create("python from PATH", "python");
 	}
 }
