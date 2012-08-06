414e423820be2bf9d7e10dab24b849abf1470568
matthew.gerring <matthew.gerring@gmail.com>
2012-08-06 09:56:04 +0100

    Added listener for temporary port.
    Conflicts:
    	uk.ac.diamond.scisoft.analysis.rcp/src/uk/ac/diamond/scisoft/analysis/rcp/AnalysisRCPActivator.java

--- uk.ac.diamond.scisoft.analysis.rcp/src/uk/ac/diamond/scisoft/analysis/rcp/AnalysisRCPActivator.java
+++ uk.ac.diamond.scisoft.analysis.rcp/src/uk/ac/diamond/scisoft/analysis/rcp/AnalysisRCPActivator.java
@@ -27,14 +27,18 @@ import org.slf4j.LoggerFactory;
 import uk.ac.diamond.scisoft.analysis.AnalysisRpcServerProvider;
 import uk.ac.diamond.scisoft.analysis.PlotServer;
 import uk.ac.diamond.scisoft.analysis.PlotServerProvider;
+import uk.ac.diamond.scisoft.analysis.RMIServerPortEvent;
+import uk.ac.diamond.scisoft.analysis.RMIServerPortListener;
 import uk.ac.diamond.scisoft.analysis.RMIServerProvider;
 import uk.ac.diamond.scisoft.analysis.rcp.preference.AnalysisRpcAndRmiPreferencePage;
+import uk.ac.diamond.scisoft.analysis.rcp.preference.PreferenceConstants;
 import uk.ac.diamond.scisoft.analysis.rpc.FlatteningService;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class AnalysisRCPActivator extends AbstractUIPlugin {
+public class AnalysisRCPActivator extends AbstractUIPlugin implements RMIServerPortListener {
+
 	private static final Logger logger = LoggerFactory.getLogger(AnalysisRCPActivator.class);
 
 	/**
@@ -63,8 +67,9 @@ public class AnalysisRCPActivator extends AbstractUIPlugin {
 		plotServerTracker = new ServiceTracker(context, PlotServer.class.getName(), null);
 		plotServerTracker.open();
 		PlotServer plotServer = (PlotServer)plotServerTracker.getService();
-		if( plotServer != null)
-			PlotServerProvider.setPlotServer(plotServer);
+		if( plotServer != null) PlotServerProvider.setPlotServer(plotServer);
+		
+		RMIServerProvider.getInstance().addPortListener(this);
 		
 		// if the rmi server has been vetoed, dont start it up, this also has issues
 		if (Boolean.getBoolean("uk.ac.diamond.scisoft.analysis.analysisrpcserverprovider.disable") == false) {
@@ -82,6 +87,19 @@ public class AnalysisRCPActivator extends AbstractUIPlugin {
 					AnalysisRpcAndRmiPreferencePage.getAnalysisRpcTempFileLocation());
 		}
 		
+	}
+	
+	/**
+	 * Simply sets properties for the temporary port if the RMIServerProvider
+	 * decides to get a temp one.
+	 */
+	@Override
+	public void portAssigned(RMIServerPortEvent evt) {
+		if (evt.isVolatilePort()) {
+			getPreferenceStore().setValue(PreferenceConstants.RMI_SERVER_PORT_AUTO, evt.getPort());
+		}else if (evt.getPort()>0) {
+			getPreferenceStore().setValue(PreferenceConstants.RMI_SERVER_PORT_AUTO, 0);
+		}
 	}
 
 	@Override
@@ -123,5 +141,6 @@ public class AnalysisRCPActivator extends AbstractUIPlugin {
 		ImageDescriptor des = imageDescriptorFromPlugin(PLUGIN_ID, path);
 		return des.createImage();
 	}
+
 }
 
