4dfc7d8ab0c2b28ada6e72dbf5d0c6243bf52228
matthew.gerring <matthew.gerring@gmail.com>
2012-08-06 17:00:27 +0100

    Added fix to save property we are using to record temp port.

--- uk.ac.diamond.scisoft.analysis.rcp/src/uk/ac/diamond/scisoft/analysis/rcp/AnalysisRCPActivator.java
+++ uk.ac.diamond.scisoft.analysis.rcp/src/uk/ac/diamond/scisoft/analysis/rcp/AnalysisRCPActivator.java
@@ -16,6 +16,15 @@
 
 package uk.ac.diamond.scisoft.analysis.rcp;
 
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.ui.PlatformUI;
@@ -87,9 +96,6 @@ public class AnalysisRCPActivator extends AbstractUIPlugin implements ServerPort
 					AnalysisRpcAndRmiPreferencePage.getAnalysisRpcTempFileLocation());
 		}
 		
-		// Seems to be needed in order to ensure class loading.
-		RMIServerProvider.getInstance().getPort();
-		AnalysisRpcServerProvider.getInstance().getPort();
 	}
 
 	@Override
@@ -97,6 +103,13 @@ public class AnalysisRCPActivator extends AbstractUIPlugin implements ServerPort
 		if (PlatformUI.isWorkbenchRunning()) { // Not workflow IApplication
 			logger.info("Setting "+PreferenceConstants.ANALYSIS_RPC_SERVER_PORT_AUTO+" to: ",  evt.getPort());
 		    getPreferenceStore().setValue(PreferenceConstants.ANALYSIS_RPC_SERVER_PORT_AUTO, evt.getPort());
+
+		    try {
+		    	IWorkspace ws = ResourcesPlugin.getWorkspace();
+		    	ws.save(true, new NullProgressMonitor());
+		    } catch (CoreException e) {
+		    	logger.error("Cannot save "+PreferenceConstants.ANALYSIS_RPC_SERVER_PORT_AUTO, e);
+		    }
 		}
 	}
 	
