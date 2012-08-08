3bddfc4ba977f027a730b3328ee302b41167e4bc
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2012-08-08 12:29:21 +0100

    SCISOFT-UI: feedback view *put all feedback classes in a feedback package *change the feedback view id *Proxy detection using ProxyVole API for automatic configured proxies *clean up some warnings

---
+++ uk.ac.diamond.scisoft/src/uk/ac/diamond/scisoft/feedback/FeedbackRequest.java
@@ -0,0 +1,109 @@
+/*-
+ * Copyright 2012 Diamond Light Source Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.ac.diamond.scisoft.feedback;
+
+import java.io.File;
+
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.params.ConnRoutePNames;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.PlatformUI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Class that submits a request to the DAWNFeedback Servlet
+ */
+public class FeedbackRequest {
+
+	private static Logger logger = LoggerFactory.getLogger(FeedbackRequest.class);
+	//this is the URL of the GAE servlet
+	private static final String SERVLET_URL = "http://dawnsci-feedback.appspot.com/dawnfeedback";
+	//proxy
+	private static String host;
+	private static int port;
+	
+
+	/**
+	 * Method used to submit a form data/file through HTTP to a GAE servlet
+	 * @param email
+	 * @param name
+	 * @param subject
+	 * @param messageBody
+	 * @param file
+	 */
+	public static void doRequest(String email, String name, String subject, String messageBody, File file) {
+
+		DefaultHttpClient httpclient = new DefaultHttpClient();
+
+		FeedbackProxy.init();
+		host = FeedbackProxy.getHost();
+		port = FeedbackProxy.getPort();
+
+		// if there is a proxy
+		if(host != null){
+			HttpHost proxy = new HttpHost(host, port, "http");
+			httpclient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
+		}
+
+		try {
+			HttpPost httpost = new HttpPost(SERVLET_URL);
+
+			MultipartEntity entity = new MultipartEntity();
+			entity.addPart("name", new StringBody(name));
+			entity.addPart("email", new StringBody(email));
+			entity.addPart("subject", new StringBody(subject));
+			entity.addPart("message", new StringBody(messageBody));
+			entity.addPart("log.html", new FileBody(file));
+			httpost.setEntity(entity);
+			
+			//HttpPost post = new HttpPost("http://dawnsci-feedback.appspot.com/dawnfeedback?name=baha&email=baha@email.com&subject=thisisasubject&message=thisisthemessage");
+			HttpResponse response = httpclient.execute(httpost);
+
+			Display.getDefault().asyncExec(new Runnable() {
+				@Override
+				public void run() {
+					MessageDialog.openInformation(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
+							"Feedback successfully sent", "Thank you for your contribution");
+				}
+			});
+			logger.debug("HTTP Response: " + response.getStatusLine());
+		} catch (Exception e) {
+			logger.error("Feedback email not sent", e);
+			Display.getDefault().asyncExec(new Runnable() {
+				@Override
+				public void run() {
+					MessageDialog.openInformation(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
+							"Feedback not sent!", "Please check that you have an Internet connection. If behind a proxy server, please check the DAWN proxy settings (Window/Preferences/General/Network Connection). If the feedback is still not working, please go to http://www.dawnsci.org (accessible through the Welcome Screen) and register your problem there (See the Contact-Us page).");
+				}
+			});
+		} finally {
+			// When HttpClient instance is no longer needed,
+			// shut down the connection manager to ensure
+			// immediate deallocation of all system resources
+			httpclient.getConnectionManager().shutdown();
+		}
+	}
+}
