06e235ac1a90b6da29a460a508efc78e7ed482f8
Karl Levik <karl.levik@diamond.ac.uk>
2012-10-02 16:41:32 +0100

    Work on DAWNSCI-111 (Editable meta-data).

--- uk.ac.diamond.sda.meta/src/uk/ac/diamond/sda/meta/tools/MetadataTool.java
+++ uk.ac.diamond.sda.meta/src/uk/ac/diamond/sda/meta/tools/MetadataTool.java
@@ -4,11 +4,15 @@ import org.dawb.common.ui.plot.tool.AbstractToolPage;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 
+import uk.ac.diamond.scisoft.analysis.io.IMetaData;
 import uk.ac.diamond.sda.meta.page.DiffractionMetadataComposite;
+
+
 
 public class MetadataTool extends AbstractToolPage {
 
 	protected DiffractionMetadataComposite diffMetadataComp;
+	protected Composite composite;
 	
 	public MetadataTool() {
 		// TODO Auto-generated constructor stub
@@ -17,9 +21,12 @@ public class MetadataTool extends AbstractToolPage {
 
 	@Override
 	public void createControl(Composite parent) {
-		// TODO Auto-generated method stub
 		diffMetadataComp = new DiffractionMetadataComposite();
-
+		diffMetadataComp.setEditable(true);
+		composite = diffMetadataComp.createComposite(parent);
+		IMetaData meta = getImageTrace().getData().getMetadata();
+		diffMetadataComp.setMetaData(meta);
+		activate();
 	}
 
 	@Override
@@ -29,8 +36,7 @@ public class MetadataTool extends AbstractToolPage {
 
 	@Override
 	public Control getControl() {
-		// TODO Auto-generated method stub
-		return null;
+		return composite;
 	}
 
 	@Override
@@ -40,8 +46,6 @@ public class MetadataTool extends AbstractToolPage {
 
 	@Override
 	public void setFocus() {
-		// TODO Auto-generated method stub
-		
+		composite.setFocus();
 	}
-
 }
